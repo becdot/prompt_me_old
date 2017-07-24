833dcc2e2d4982ea14e268b7c2e0299c5867ff2d
becdot <becca.liss@gmail.com>
2017-07-24 16:03:28 -0400

    Update tests

--- tests/functional/prompt_test.js
+++ tests/functional/prompt_test.js
@@ -1,19 +1,31 @@
 const assert = require('chai').assert;
-require('dotenv').config({ path: './../../.promptsrc' });
 const request = require('request');
+require('dotenv').config({ path: './.promptsrc' });
 
-// require('./../../index');
+const Prompt = require('./../../prompt/prompt');
 
 describe('prompt', () => {
   const self = this;
 
   before(() => {
     self.url = `http://localhost:${process.env.PORT}`;
+    self.ids = [];
+    const promptData = [{ text: 'Prompt 1.' }, { text: 'Prompt 2.' }];
+    const model = Prompt.getModel();
+    return Prompt.connect().
+      then(() => model.create(promptData)).
+      then(prompts => prompts.forEach(p => self.ids.push(p._id))).
+      then(Prompt.close);
   });
 
-  const makeRequest = (method, uri) => {
+  after(() => {
+    const model = Prompt.getModel();
+    return Prompt.connect().then(() => model.remove({})).then(Prompt.close);
+  });
+
+  const makeRequest = (options) => {
     return new Promise((resolve, reject) => {
-      return request({ method, uri }, (err, resp, body) => {
+      return request(options, (err, resp, body) => {
         if (err) {
           reject(err);
         } else {
@@ -26,7 +38,8 @@ describe('prompt', () => {
   describe('/prompts', () => {
     describe('GET', () => {
       before(() => {
-        return makeRequest('GET', `${self.url}/prompts`).then(([response, body]) => {
+        const data = { method: 'GET', uri: `${self.url}/prompts` };
+        return makeRequest(data).then(([response, body]) => {
           self.response = response;
           self.body = JSON.parse(body);
         });
@@ -36,56 +49,102 @@ describe('prompt', () => {
         assert.equal(200, self.response.statusCode);
       });
 
-      it('should return an array of prompts', () => {
+      it('should return a non-empty array of prompts', () => {
         assert.isArray(self.body);
-      });
-
-      it('should return at least one prompt', () => {
-        const prompt = self.body[0];
-        assert.exists(prompt);
-        assert.exists(prompt._id);
-        assert.exists(prompt.text);
+        assert.notEmpty(self.body);
       });
     });
 
+    describe('POST', () => {
+      before(() => {
+        const data = { method: 'POST', uri: `${self.url}/prompts`, json: true, body: { text: 'A prompt.' } };
+        return makeRequest(data).then(([response, body]) => {
+          self.response = response;
+          self.body = body;
+        });
+      });
 
-    // curl -X POST http://localhost:3000/prompts -H "Content-Type:application/json" -d '{"text": "Spend five minutes describing an unusual pet"}'
-    // describe('POST', () => {
-    //   before(() => {
-    //     return makeRequest('POST', `${self.url}/prompts`).then(([response, body]) => {
-    //       self.response = response;
-    //       self.body = JSON.parse(body);
-    //     });
-    //   });
+      it('should make a successful request', () => {
+        assert.equal(200, self.response.statusCode);
+      });
 
-    //   it('should make a successful request', () => {
-    //     assert.equal(200, self.response.statusCode);
-    //   });
-
-    //   it('should return an array of prompts', () => {
-    //     assert.isArray(self.body);
-    //   });
-
-    //   it('should return at least one prompt', () => {
-    //     const prompt = self.body[0];
-    //     assert.exists(prompt);
-    //     assert.exists(prompt._id);
-    //     assert.exists(prompt.text);
-    //   });
-    // });
+      it('should return the ID of the new prompt', () => {
+        assert.ok(self.body._id);
+      });
+    });
   });
 
-  // describe('/prompts/:id', () => {
-  //   describe('GET', () => {
+  describe('/prompts/:id', () => {
+    describe('GET', () => {
+      before(() => {
+        const data = { method: 'GET', uri: `${self.url}/prompts/${self.ids[0]}` };
+        return makeRequest(data).then(([response, body]) => {
+          self.response = response;
+          self.body = JSON.parse(body);
+        });
+      });
 
-  //   });
+      it('should make a successful request', () => {
+        assert.equal(200, self.response.statusCode);
+      });
 
-  //   describe('PUT', () => {
+      it('should return the correct prompt', () => {
+        assert.equal(self.ids[0], self.body._id);
+        assert.equal('Prompt 1.', self.body.text);
+      });
+    });
 
-  //   });
+    describe('PUT', () => {
+      before(() => {
+        const data = { method: 'PUT', uri: `${self.url}/prompts/${self.ids[0]}`, json: true, body: { text: 'A way cooler prompt!' } };
+        return makeRequest(data).then(([response, body]) => {
+          self.response = response;
+          self.body = body;
+        });
+      });
 
-  //   describe('DELETE', () => {
+      it('should make a successful request', () => {
+        assert.equal(200, self.response.statusCode);
+      });
 
-  //   });
-  // });
+      it('should return the old prompt text', () => {
+        assert.equal(self.ids[0], self.body._id);
+        assert.equal('Prompt 1.', self.body.text);
+      });
+
+      it('should be able to query for the new text', () => {
+        return Prompt.find({ _id: self.body._id }).then((prompt) => {
+          assert.equal('A way cooler prompt!', prompt.text);
+        });
+      });
+    });
+
+    describe('DELETE', () => {
+      before(() => {
+        const data = { method: 'DELETE', uri: `${self.url}/prompts/${self.ids[0]}` };
+
+        return new Promise(resolve => {
+          makeRequest(data).then(([response, body]) => {
+            self.response = response;
+            self.body = JSON.parse(body);
+            resolve();
+          });
+        });
+      });
+
+      it('should make a successful request', () => {
+        assert.equal(200, self.response.statusCode);
+      });
+
+      it('should return the deleted prompt text', () => {
+        assert.equal(self.ids[0], self.body._id);
+      });
+
+      it('should not be able to find the deleted model', () => {
+        return Prompt.find({ _id: self.ids[0] }).then(prompt => {
+          assert.notOk(prompt);
+        });
+      });
+    });
+  });
 });
